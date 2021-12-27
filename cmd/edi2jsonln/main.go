852c0e4dddbde71f035558f1335b095bf956b502
conni <cbuschka@gmail.com>
2021-12-27 11:09:51 +0100

    Added command edi2jsonln.

---
+++ cmd/edi2jsonln/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/edilib/go-edi/internal/edi2jsonln"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	fqProg := os.Args[0]
+	prog := filepath.Base(fqProg)
+	err := edi2jsonln.Run(prog, os.Args[1:])
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed: %s}n", err.Error())
+		os.Exit(1)
+	}
+}
